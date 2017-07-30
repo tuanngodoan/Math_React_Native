56bb049611e2236d08bf161146ef543a012828b1
Doan Tuan <tuannd@hblab.vn>
2017-07-30 13:29:51 +0700

    test fork

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class Math_React_Native extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Math for KID
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
